5807349c34a3ba0d95be378b8f92a791baece92f
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-10-31 17:16:57 -0600

    Nuevos sp para el sistema de los timbrados automaticos

---
+++ Stored Procedures/dbo.sp_Total_Order_Oct_2022_JC.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Order_Oct_2022_JC] (@nfecha varchar(100))
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+        SELECT COUNT(*) AS total,MONTH(try_CONVERT(DATETIME, fechaTimbrado)) AS MES FROM Reporte_Timbradas WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 8 AND YEAR(try_CONVERT(DATETIME, fechaTimbrado)) = @nfecha GROUP BY MONTH(try_CONVERT(DATETIME, fechaTimbrado))
+END
+GO
---
+++ Stored Procedures/dbo.sp_Total_Order_Octubre_Dhl_year_JC.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Order_Octubre_Dhl_year_JC] (@nfecha varchar(100))
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+        SELECT COUNT(*) AS total,MONTH(try_CONVERT(DATETIME, fechaTimbrado)) AS MES FROM RtDhl WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 10 AND YEAR(try_CONVERT(DATETIME, fechaTimbrado)) = @nfecha GROUP BY MONTH(try_CONVERT(DATETIME, fechaTimbrado))
+END
+GO
