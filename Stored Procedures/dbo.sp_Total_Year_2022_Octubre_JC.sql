31e1e00fa505df1194ac8ce66f0cbae7de6719dc
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-11-03 17:59:34 -0600

    Nuevos sp para detalles por mes del sistema de timbrado automatico

---
+++ Stored Procedures/dbo.sp_Total_Year_2022_Octubre_JC.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Year_2022_Octubre_JC] 
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+    SELECT orden, segmento, billto, fechaTimbrado FROM Reporte_Timbradas WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 10 AND YEAR(try_CONVERT(DATETIME, fechaTimbrado)) = '2022' GROUP BY orden, segmento, billto, fechaTimbrado ORDER BY fechaTimbrado DESC         
+END
+GO
