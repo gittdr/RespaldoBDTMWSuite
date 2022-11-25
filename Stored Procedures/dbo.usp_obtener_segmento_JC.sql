04949a7387722a391153716129f19ff673b86760
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-11-25 17:58:51 -0600

    Modificacion al sp_obtener_segmento_JC

--- Stored Procedures/dbo.usp_obtener_segmento_JC.sql
+++ Stored Procedures/dbo.usp_obtener_segmento_JC.sql
@@ -8,7 +8,7 @@ CREATE procedure [dbo].[usp_obtener_segmento_JC](
 as
 
 begin
-        Select h.lgh_number as Segmento ,v.Folio as Folio, v.Fecha as Fechatimbrado, o.ord_billto as cliente, h.ord_hdrnumber as orden, v.cancelfactura cancelada , v.pdf_descargafactura PDF
+        Select TOP 1 h.lgh_number as Segmento ,v.Folio as Folio, v.Fecha as Fechatimbrado, o.ord_billto as cliente, h.ord_hdrnumber as orden, v.cancelfactura cancelada , v.pdf_descargafactura PDF
         from legheader as h LEFT JOIN VISTA_Carta_Porte AS v ON v.Folio = h.lgh_number LEFT JOIN orderheader as o ON o.ord_hdrnumber = h.ord_hdrnumber WHERE  v.Folio != 'NULL' AND o.ord_startdate > '2022-03-01' AND h.lgh_outstatus != 'CAN' AND h.ord_hdrnumber > 0 AND h.lgh_number in (	select lgh_number from legheader where ord_hdrnumber = @orden)
 end
 GO
