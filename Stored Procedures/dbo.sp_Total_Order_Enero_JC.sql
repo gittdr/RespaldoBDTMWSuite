a1fd53231bbf2021257772d07983d6a079805996
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-12-26 17:41:59 -0600

    Nuevos sp para el sistema de reportes de timbrado automatico

---
+++ Stored Procedures/dbo.sp_Total_Order_Enero_JC.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Order_Enero_JC]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+        SELECT COUNT(*) AS total,MONTH(try_CONVERT(DATETIME, fechaTimbrado)) AS MES FROM Reporte_Timbradas WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 01 GROUP BY MONTH(try_CONVERT(DATETIME, fechaTimbrado))
+END
+GO
---
+++ Stored Procedures/dbo.sp_Total_Year_Enero_2023_JC.sql
@@ -0,0 +1,14 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Year_Enero_2023_JC] 
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+    SELECT TOP 1
+(SELECT COUNT(*) AS total FROM Reporte_Timbradas WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 01 AND YEAR(try_CONVERT(DATETIME, fechaTimbrado)) = '2023' GROUP BY MONTH(try_CONVERT(DATETIME, fechaTimbrado))) AS ENERO
+FROM Reporte_Timbradas        
+END
+GO
