86a726a4a6f908934bca203be80928d20ce9d370
Julio Herrera <jcherrera@bgcapitalgroup.mx>
2022-12-29 17:01:35 -0600

    Nuevos sp del sistema de reporte de timbrado automatico

---
+++ Stored Procedures/dbo.sp_NotificacionesEleosImages.sql
@@ -0,0 +1,33 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_NotificacionesEleosImages](@leg varchar(100),@titulo varchar(100),@mensaje varchar(8000))
+	
+AS
+BEGIN
+DECLARE @bodyC varchar(8000)
+DECLARE @subjectC varchar(1000)
+DECLARE @files varchar(500)
+DECLARE @copy_to varchar(100)
+
+
+set @bodyC = '';
+set @subjectC = @leg;
+set @files = @titulo;
+set @copy_to = 'jcherrera@bgcapitalgroup.mx';
+
+BEGIN
+	
+	EXEC msdb.dbo.sp_send_dbmail  
+    @profile_name = 'ApiEleos',  
+    --@recipients = 'lbarron@convoy360.mx',  
+	@recipients = 'jcherrera@bgcapitalgroup.mx',  
+    --@body = 'DX > 500 Alerta de creacion masiva de Ordenes!!!!!', 
+	@body = @bodyC ,
+	@file_attachments = @files,
+    @subject = @subjectC; 
+
+END
+END
+GO
---
+++ Stored Procedures/dbo.sp_Total_Year_2023_Enero_Penafiel_JC.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_Total_Year_2023_Enero_Penafiel_JC] 
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+    SELECT orden, segmento, billto, fechaTimbrado FROM RtPenafiel WHERE fechaTimbrado is not null AND MONTH(try_CONVERT(DATETIME, fechaTimbrado)) = 01 AND YEAR(try_CONVERT(DATETIME, fechaTimbrado)) = '2023' GROUP BY orden, segmento, billto, fechaTimbrado ORDER BY fechaTimbrado DESC         
+END
+GO
